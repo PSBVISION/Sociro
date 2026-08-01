0d0a60cc0b012845dbc7b650fd88e7603497ca2a
PSBVISION <punitsinghbisht1529@gmail.com>
2026-08-01 23:12:54 +0530

    feat: enhance Sidebar component with responsive styles and transitions

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,7 +1,11 @@
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
+  return
+<div className={fixed inset-y-0 left-0 z-50 w-64
+flex flex-col h-full transform transition-transform duration-200 ease-in-out
+md:relative md:translate-x-0'}>
+
+lbg-white border-r border-slate-200">Sidebar</div>
   )
 }
 
