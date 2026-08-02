22a6a1550354a4aa796b84b276c75eb6199449bc
PSBVISION <punitsinghbisht1529@gmail.com>
2026-08-02 23:02:37 +0530

    fix: correct formatting and syntax in Sidebar component

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,12 +1,11 @@
-
 const Sidebar = () => {
-  return
-<div className={fixed inset-y-0 left-0 z-50 w-64
-flex flex-col h-full transform transition-transform duration-200 ease-in-out
-md:relative md:translate-x-0'}>
+  return (
+    <div
+      className={`fixed inset-y-0 left-0 z-50 w-64flex flex-col h-full transform transition-transform duration-200 ease-in-out md:relative md:translate-x-0 bg-white border-r border-slate-200`}
+    >
+      Sidebar
+    </div>
+  );
+};
 
-lbg-white border-r border-slate-200">Sidebar</div>
-  )
-}
-
-export default Sidebar
+export default Sidebar;
